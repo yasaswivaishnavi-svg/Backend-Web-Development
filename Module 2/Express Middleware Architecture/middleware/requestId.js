64118222462794abb97e2dd5c12bbc089ca62b52
yasaswivaishnavi-svg <yasaswi.vaishnavi@kalvium.com>
2026-09-05 12:20:33 +0530

    requestId.js

--- Module 2/Express Middleware Architecture/middleware/requestId.js
+++ Module 2/Express Middleware Architecture/middleware/requestId.js
@@ -1,15 +1,12 @@
-/**
- * requestId middleware  [mount GLOBALLY in app.js]
- *
- * TODO: export a middleware function (req, res, next) that:
- *   - generates a UUID with crypto.randomUUID()
- *   - attaches it to req.id            (so later middleware/handlers can read it)
- *   - sets it as the "X-Request-Id" response header
- *   - calls next() so the request continues down the pipeline
- */
+const crypto = require('crypto');
 
-const { randomUUID } = require('crypto');
+function requestId(req, res, next) {
+  const id = crypto.randomUUID();
 
-module.exports = function requestId(req, res, next) {
-  // TODO: implement the four steps described above.
-};
+  req.id = id;
+  res.setHeader('X-Request-Id', id);
+
+  next();
+}
+
+module.exports = requestId;
